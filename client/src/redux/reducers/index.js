f4ecf8086c2095f3f7934334613bf483efdd760f
Donald Tang <turazinyc@gmail.com>
2021-08-18 22:10:56 -0400

    Added action and reducer files to search for food results and util function to extract categories from results

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 
+import results from './results';
+
 const rootReducer = combineReducers({
+    results: results
 });
 
 export default rootReducer;
---
+++ client/src/utils/resultsOperations.js
@@ -0,0 +1,23 @@
+// return list of categories after retrieving results
+exports.getCategoriesFromResults = (data) => {
+    let categories = {};
+    let categoriesArr = [];
+    for (let result of data) {
+        let catArr = result.categories;
+        for (let cat of catArr) {
+            if (categories[cat.title]) {
+                categories[cat.title].count = categories[cat.title].count + 1;
+            } else {
+                categories[cat.title] = { 
+                    alias: cat.alias,
+                    title: cat.title,
+                    count: 1
+                }
+            }
+        }
+    }
+    for (let catObj in categories) {
+        categoriesArr.push(categories[catObj]);
+    }
+    return categoriesArr;
+}
